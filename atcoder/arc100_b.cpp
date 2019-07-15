b1f865d74533d1550c56aa11d49037b4173b1311
en30 <en30.git@gmail.com>
2019-07-16 02:26:06 +0900

    ABC102 D しゃくとり法

--- atcoder/arc100_b.cpp
+++ atcoder/arc100_b.cpp
@@ -8,22 +8,9 @@ typedef pair<ll, ll> res;
 int N;
 vector<ll> A, S;
 
-// [l, r)
-res minimize(int l0, int r0) {
-  ll s = S[r0] - S[l0];
-  int l = l0, r = r0;
-  while(r - l > 1) {
-    int m = (l+r)/2;
-    if(S[m+1] - S[l0] <= s/2) {
-      l = m;
-    } else {
-      r = m;
-    }
-  }
-  if(r0 > l + 2 && abs(2*S[l+1]- S[l0] - S[r0]) > abs(2*S[l+2]- S[l0] - S[r0])) l++;
-  ll x = S[l+1] - S[l0];
-  ll y = S[r0] - S[l+1];
-  return res(min(x,y), max(x,y));
+// [a, b), [b, c)
+ll sumdiff(int a, int b, int c) {
+  return abs(S[c] - 2 * S[b] + S[a]);
 }
 
 int main () {
@@ -36,11 +23,13 @@ int main () {
   }
 
   ll ans = 1e15;
-  res rl, rr;
-  for(int i = 1; i < N-2; i++) {
-    rl = minimize(0, i+1);
-    rr = minimize(i+1, N);
-    ans = min(ans, max(rl.second, rr.second) - min(rl.first, rr.first));
+  int p = 1, r = 3;
+  for(int q = 2; q < N - 1; q++) {
+    while(sumdiff(q, r+1, N) < sumdiff(q, r, N)) r++;
+    while(sumdiff(0, p+1, q) < sumdiff(0, p, q)) p++;
+    vector<ll> B = {S[p]-S[0], S[q]-S[p], S[r]-S[q], S[N]-S[r]};
+    sort(B.begin(), B.end());
+    ans = min(ans, B[3] - B[0]);
   }
 
   cout << ans << endl;
