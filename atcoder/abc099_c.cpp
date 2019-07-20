dfd0973c7a370b5ff239ce9a922d51f228dba0c5
en30 <en30.git@gmail.com>
2019-07-20 15:44:34 +0900

    ABC099 C editorial
    https://atcoder.jp/contests/abc099/submissions/6441734

--- atcoder/abc099_c.cpp
+++ atcoder/abc099_c.cpp
@@ -4,30 +4,28 @@ typedef long long ll;
 #define rep(i, N) for (int i = 0; i < (int)N; i++)
 
 const int INF = 1e9;
+
+int f(int n, int r) {
+  int res = 0;
+
+  while(n != 0) {
+    res += n % r;
+    n /= r;
+  }
+
+  return res;
+}
+
 int main () {
   int N;
   cin >> N;
 
-  vector<int> v;
-  v.push_back(1);
-  for(int x = 6; x <= N; x *= 6) {
-    v.push_back(x);
-  }
-  for(int x = 9; x <= N; x *= 9) {
-    v.push_back(x);
+  int ans = INF;
+  for(int i = 0; i <= N; i++) {
+    ans = min(ans, f(i, 6) + f(N-i, 9));
   }
 
-  int M = v.size();
-  vector<vector<int>> dp(M+1, vector<int>(N+1, INF));
-  dp[0][0] = 0;
-  for(int i = 0; i < M; i++) {
-    for(int j = 0; j <= N; j++) {
-      dp[i+1][j] = dp[i][j];
-      if(j-v[i] >= 0) dp[i+1][j] = min(dp[i+1][j], dp[i+1][j-v[i]] + 1);
-    }
-  }
-
-  cout << dp[M][N] << endl;
+  cout << ans << endl;
 
   return 0;
 }
