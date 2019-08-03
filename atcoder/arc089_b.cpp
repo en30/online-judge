507420806f2fef93cc1cd62282046c0dbef854d9
en30 <en30.git@gmail.com>
2019-08-03 17:09:52 +0900

    ABC089 D bとwの条件をまとめ

--- atcoder/arc089_b.cpp
+++ atcoder/arc089_b.cpp
@@ -10,37 +10,40 @@ int main () {
   cin >> N >> K;
 
   int T = 2*K;
-  vector<vector<vector<int>>> C(2, vector<vector<int>>(T, vector<int>(T, 0)));
+  vector<vector<int>> C(T, vector<int>(T, 0));
 
   rep(i,N) {
     int x, y;
     char c;
     cin >> x >> y >> c;
-    C[c == 'B'][y%T][x%T]++;
+    if(c == 'W') x += K;
+    C[y%T][x%T]++;
   }
 
-  vector<vector<vector<int>>> SC(2, vector<vector<int>>(T+1, vector<int>(T+1, 0)));
-  rep(c, 2) rep(i,T) rep(j,T) {
-    SC[c][i+1][j+1] = C[c][i][j] + SC[c][i+1][j] + SC[c][i][j+1] - SC[c][i][j];
+  vector<vector<int>> SC(T+1, vector<int>(T+1, 0));
+  rep(i,T) rep(j,T) {
+    SC[i+1][j+1] = C[i][j] + SC[i+1][j] + SC[i][j+1] - SC[i][j];
   }
 
   // [x0, x1) x [y0, y1)
-  auto rangeSum = [&](int c, int y0, int y1, int x0, int x1) {
-    return SC[c][y1][x1] - SC[c][y0][x1] - SC[c][y1][x0] + SC[c][y0][x0];
+  auto rangeSum = [&](int y0, int y1, int x0, int x1) {
+    return SC[y1][x1] - SC[y0][x1] - SC[y1][x0] + SC[y0][x0];
   };
 
   int ans = 0;
   rep(i,T) rep(j,T) {
-    int flip = (j < K) ^ (i < K);
     int x1 = (j+K) % T, x2 = j;
     if(x1 > x2)swap(x1, x2);
     int y1 = (i+K) % T, y2 = i;
     if(y1 > y2)swap(y1, y2);
 
-    ans = max(ans,
-              rangeSum(flip^1, 0, y1, 0, x1) + rangeSum(flip^0, 0, y1, x1, x2) + rangeSum(flip^1, 0, y1, x2, T) +
-              rangeSum(flip^0, y1, y2, 0, x1) + rangeSum(flip^1, y1, y2, x1, x2) + rangeSum(flip^0, y1, y2, x2, T) +
-              rangeSum(flip^1, y2, T, 0, x1) + rangeSum(flip^0, y2, T, x1, x2) + rangeSum(flip^1, y2, T, x2, T));
+    int b = rangeSum(0, y1, 0, x1)
+      + rangeSum(0, y1, x2, T)
+      + rangeSum(y1, y2, x1, x2)
+      + rangeSum(y2, T, 0, x1)
+      + rangeSum(y2, T, x2, T);
+
+    ans = max(ans, max(b, N - b));
   }
 
   cout << ans << endl;
