bbf3ba94bd35c67b997d0f286a6e2704cdaa6425
en30 <en30.git@gmail.com>
2019-08-11 23:17:56 +0900

    ABC078 D O(1)

--- atcoder/arc085_b.cpp
+++ atcoder/arc085_b.cpp
@@ -7,56 +7,15 @@ typedef long long ll;
 int N, X, Y;
 vector<int> a;
 
-int fx(int x, int y, int i);
-int fy(int x, int y, int i);
-
-unordered_map<int, unordered_map<int, unordered_map<int, int>>> mx, my;
-
-int fx(int x, int y, int i) {
-  if(i == N) return abs(x-y);
-
-  if(mx[x][y].find(i) != mx[x][y].end()) return mx[x][y][i];
-
-  int k = i;
-  int m = -1;
-  for(int j = i; j < N; j++) {
-    int v = fy(a[j], y, j+1);
-    if(v > m) {
-      k = j;
-      m = v;
-    }
-  }
-
-  mx[x][y][i] = m;
-  return m;
-}
-
-int fy(int x, int y, int i) {
-  if(i == N) return abs(x-y);
-
-  if(my[x][y].find(i) != my[x][y].end()) return my[x][y][i];
-
-  int k = i;
-  int m = 1e9 + 1;
-  for(int j = i; j < N; j++) {
-    int v = fx(x, a[j], j+1);
-    if(v < m) {
-      k = j;
-      m = v;
-    }
-  }
-
-  my[x][y][i] = m;
-  return m;
-}
-
 int main () {
   cin >> N >> X >> Y;
   a.resize(N);
 
   rep(i,N) cin >> a[i];
 
-  cout << fx(X, Y, 0) << endl;
+  int ans = abs(a[N-1] - Y);
+  if(N > 1) ans = max(ans, abs(a[N-1] - a[N-2]));
+  cout << ans << endl;
 
   return 0;
 }
