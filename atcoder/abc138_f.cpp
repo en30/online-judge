2540074af642424fe87751ec880a7e344bf61a04
en30 <en30.git@gmail.com>
2019-09-01 05:42:05 +0900

    ABC138 F 解説AC

--- atcoder/abc138_f.cpp
+++ atcoder/abc138_f.cpp
@@ -73,53 +73,38 @@ template<int MOD> struct Fp {
 
 using mint = Fp<MOD>;
 
-const int D = 64;
-
-vector<mint> fact(D, 1), ifact(D, 1);
-
-mint comb(int n, int k) {
-  return fact[n] * ifact[n-k] * ifact[k];
-}
-
-mint f(ll x) {
-  vector<mint> c(D, 0);
-  int m = D - 1;
-  while(m >= 0 && (((x >> m) & 1) == 0)) m--;
-  for(int i = 0; i < m - 1; i++) {
-    for(int j = 0; j <= i; j++) {
-      c[j] += comb(i, j);
-    }
-  }
-
-  int k = 0;
-  while(m >= 0) {
-    m--;
-    while(m >= 0 && (((x >> m) & 1) == 0)) {
-      k++;
-      m--;
-    }
-    for(int i = 0; i < m; i++) {
-      c[i+k+1] += comb(m, i);
-    }
-  }
-
-  mint ans = 0;
-  rep(i,D) {
-    ans += modpow(mint(2), i) * c[i];
-  }
-  return ans;
-}
+mint dp[61][2][2][2];
 
 int main () {
   cin >> L >> R;
 
-  for(int i = 1; i < D; i++) {
-    fact[i] = fact[i-1] * i;
-  }
-  rep(i,D) ifact[i] = modinv(fact[i]);
+  dp[60][0][0][0] = 1;
+  for(int i = 59; i >= 0; i--) {
+    int lb = (L >> i) & 1;
+    int rb = (R >> i) & 1;
 
-  printf("f(%lld) = %d\n", R+1, f(R+1));
-  printf("f(%lld) = %d\n", L, f(L));
-  cout << f(R+1) - f(L) << endl;
+    rep(j,2)rep(k,2)rep(l,2) {
+      mint pre = dp[i+1][j][k][l];
+      rep(x,2) rep(y,2) {
+        if(x && !y) continue;
+
+        int nj = j, nk = k, nl = l;
+        if(!l && x != y) continue;
+        if(x && y) nl = 1;
+
+        if(!j && !x && lb) continue;
+        if(x && !lb) nj = 1;
+
+        if(!k && y && !rb) continue;
+        if(!y && rb) nk = 1;
+
+        dp[i][nj][nk][nl] += pre;
+      }
+    }
+  }
+
+  mint ans = 0;
+  rep(j,2)rep(k,2)rep(l,2) ans += dp[0][j][k][l];
+  cout << ans << endl;
   return 0;
 }
