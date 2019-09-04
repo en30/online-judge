ef51f2090d786f8b27bd59331cbe77157611ff6a
en30 <en30.git@gmail.com>
2019-09-05 05:59:03 +0900

    ABC139 F

--- atcoder/abc139_f.cpp
+++ atcoder/abc139_f.cpp
@@ -7,19 +7,40 @@ typedef long long ll;
 int N;
 
 struct V {
-  double x, y, t;
+  ll x, y;
 
-  V(double x = 0, double y = 0): x(x), y(y), t(atan2(y,x)) {
-  };
+  V(ll x = 0, ll y = 0): x(x), y(y) {};
 
-  bool operator < (const V &that) {
-    return t < that.t;
+  ll r2() const {
+    return dot(*this);
   }
 
   V& operator += (const V &v) {
     x += v.x;
     y += v.y;
     return *this;
+  }
+
+  ll cross(const V& that) const {
+    return x * that.y - that.x * y;
+  }
+
+  ll dot(const V& that) const {
+    return x * that.x + y * that.y;
+  }
+
+  int orthant() const {
+    if(x == 0 && y == 0) return 0;
+    if(x > 0 && y >= 0) return 1;
+    if(x <= 0 && y > 0) return 2;
+    if(x < 0 && y <= 0) return 3;
+    return 4;
+  }
+
+  bool operator < (const V &that) const {
+    int o1 = orthant(), o2 = that.orthant();
+    if(o1 != o2) return o1 < o2;
+    return cross(that) > 0;
   }
 };
 
@@ -29,19 +50,19 @@ int main () {
   cin >> N;
 
   rep(i,N) {
-    double a, b;
+    ll a, b;
     cin >> a >> b;
     x.emplace_back(a, b);
   }
 
   sort(all(x));
 
-  double ans = 0;
+  ll ans = 0;
   rep(i,N) {
     V c(0, 0);
     rep(j,N) {
       c += x[(i+j)%N];
-      ans = max(ans, c.x*c.x + c.y*c.y);
+      ans = max(ans, c.r2());
     }
   }
 
