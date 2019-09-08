34da33618aaae22c7973c5fdd653aaa5602b5260
en30 <en30.git@gmail.com>
2019-09-09 04:57:51 +0900

    simplifies ABC140 D

--- atcoder/abc140_d.cpp
+++ atcoder/abc140_d.cpp
@@ -14,20 +14,11 @@ int main() {
     d[i] = (c == 'L' ? 0 : 1);
   }
 
-  int ans = 0;
+  int score = 0;
   rep(i, N - 1) {
-    if (d[i + 1] == d[i]) ++ans;
+    if (d[i + 1] == d[i]) ++score;
   }
 
-  int a = 0, b = 0;
-  rep(i, N - 1) {
-    if (d[i] == 0 && d[i + 1] == 1) ++a;
-    if (d[i] == 1 && d[i + 1] == 0) ++b;
-  }
-
-  ans += 2 * min({a, b, K});
-  if (K > min(a, b) && a != b) ans += 1;
-
-  cout << ans << endl;
+  cout << min(score + 2 * K, N - 1) << endl;
   return 0;
 }
