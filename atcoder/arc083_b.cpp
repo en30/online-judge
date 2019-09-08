eb890f57af592d133631590a56c9830bba4dfb25
en30 <en30.git@gmail.com>
2019-09-09 00:49:26 +0900

    adds .clang-format

--- atcoder/arc083_b.cpp
+++ atcoder/arc083_b.cpp
@@ -7,30 +7,30 @@ int N;
 vector<vector<int>> A;
 
 bool hasBypaass(int u, int v) {
-  rep(w,N) {
-    if(u == w || v == w) continue;
-    if(A[u][v] == A[u][w] + A[w][v]) return true;
+  rep(w, N) {
+    if (u == w || v == w) continue;
+    if (A[u][v] == A[u][w] + A[w][v]) return true;
   }
   return false;
 }
 
-int main () {
+int main() {
   cin >> N;
   A.resize(N, vector<int>(N));
-  rep(i,N)rep(j,N) cin >> A[i][j];
+  rep(i, N) rep(j, N) cin >> A[i][j];
 
   FloydWarshall<int> fw(A);
 
   ll ans = 0;
 
-  for(int u = 0; u < N; ++u) {
-    for(int v = u + 1; v < N; ++v) {
-      if(fw.dist(u, v) < A[u][v]) {
+  for (int u = 0; u < N; ++u) {
+    for (int v = u + 1; v < N; ++v) {
+      if (fw.dist(u, v) < A[u][v]) {
         cout << "-1" << endl;
         return 0;
       }
 
-      if(!hasBypaass(u, v)) ans += A[u][v];
+      if (!hasBypaass(u, v)) ans += A[u][v];
     }
   }
 
--- include/floyd_warshall.hpp
+++ include/floyd_warshall.hpp
@@ -1,18 +1,17 @@
-template<typename T>
+template <typename T>
 class FloydWarshall {
   bool _hasNegativeCycle;
   vector<vector<T>> distTo;
 
-public:
-
+ public:
   FloydWarshall(vector<vector<T>> G) {
     int V = G.size();
     T INF = numeric_limits<T>::max() / 2;
     distTo.resize(V, vector<T>(V, INF));
 
-    for(int u = 0; u < V; ++u) {
-      for(int v = 0; v < V; ++v) {
-        if(u == v) {
+    for (int u = 0; u < V; ++u) {
+      for (int v = 0; v < V; ++v) {
+        if (u == v) {
           distTo[u][v] = 0;
           continue;
         }
@@ -21,13 +20,13 @@ public:
       }
     }
 
-    for(int w = 0; w < V; ++w) {
-      for(int u = 0; u < V; ++u) {
-        for(int v = 0; v < V; ++v) {
+    for (int w = 0; w < V; ++w) {
+      for (int u = 0; u < V; ++u) {
+        for (int v = 0; v < V; ++v) {
           distTo[u][v] = min(distTo[u][v], distTo[u][w] + distTo[w][v]);
         }
 
-        if(distTo[u][u] < 0) {
+        if (distTo[u][u] < 0) {
           _hasNegativeCycle = true;
           return;
         }
@@ -35,11 +34,7 @@ public:
     }
   }
 
-  bool hasNegativeCycle() {
-    return _hasNegativeCycle;
-  }
+  bool hasNegativeCycle() { return _hasNegativeCycle; }
 
-  T dist(int s, int t) {
-    return distTo[s][t];
-  }
+  T dist(int s, int t) { return distTo[s][t]; }
 };
