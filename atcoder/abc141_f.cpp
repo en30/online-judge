25e38218b00a4322cf7f16d390bb0e172541bf63
en30 <en30.git@gmail.com>
2019-09-17 02:49:47 +0900

    ABC141 F bitset

--- atcoder/abc141_f.cpp
+++ atcoder/abc141_f.cpp
@@ -1,27 +1,33 @@
 #include <bits/stdc++.h>
 #include "../include/template"
 
+typedef bitset<64> bs;
+
 int N;
-vector<ll> A;
+vector<bs> A;
 
 int main() {
   cin >> N;
   A.resize(N);
-  rep(i, N) cin >> A[i];
+  rep(i, N) {
+    ll a;
+    cin >> a;
+    A[i] = bs(a);
+  }
 
   const int D = 60;
-  ll allXor = 0;
+  bs allXor;
   rep(i, N) allXor ^= A[i];
   rep(i, N) A[i] &= ~allXor;
 
   int idx = 0;
   for (int d = D - 1; d >= 0; --d) {
     int j = idx;
-    while (j < N && (((A[j] >> d) & 1) == 0)) ++j;
+    while (j < N && !A[j][d]) ++j;
     if (j < N) {
       swap(A[idx], A[j]);
       rep(k, N) {
-        if (k != idx && ((A[k] >> d) & 1)) {
+        if (k != idx && A[k][d]) {
           A[k] ^= A[idx];
         }
       }
@@ -29,10 +35,10 @@ int main() {
     }
   }
 
-  ll ans = 0;
-  rep(i, N) ans = max(ans, ans ^ A[i]);
+  unsigned long long ans = 0;
+  rep(i, N) ans = max(ans, ans ^ A[i].to_ullong());
   ans <<= 1;
-  ans += allXor;
+  ans += allXor.to_ullong();
 
   cout << ans << endl;
 
