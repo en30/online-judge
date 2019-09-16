cf26e919b3063407f0a1e5c43232d785ec96a598
en30 <en30.git@gmail.com>
2019-09-17 03:34:57 +0900

    ABC141 E dp

--- atcoder/abc141_e.cpp
+++ atcoder/abc141_e.cpp
@@ -4,86 +4,24 @@
 int N;
 string S;
 
-template <unsigned mod>
-struct RollingHash {
-  vector<unsigned> hashed, power;
-
-  inline unsigned mul(unsigned a, unsigned b) const {
-    unsigned long long x = (unsigned long long)a * b;
-    unsigned xh = (unsigned)(x >> 32), xl = (unsigned)x, d, m;
-    asm("divl %4; \n\t" : "=a"(d), "=d"(m) : "d"(xh), "a"(xl), "r"(mod));
-    return m;
-  }
-
-  RollingHash(const string &s, unsigned base = 10007) {
-    int sz = (int)s.size();
-    hashed.assign(sz + 1, 0);
-    power.assign(sz + 1, 0);
-    power[0] = 1;
-    for (int i = 0; i < sz; i++) {
-      power[i + 1] = mul(power[i], base);
-      hashed[i + 1] = mul(hashed[i], base) + s[i];
-      if (hashed[i + 1] >= mod) hashed[i + 1] -= mod;
-    }
-  }
-
-  unsigned get(int l, int r) const {
-    unsigned ret = hashed[r] + mod - mul(hashed[l], power[r - l]);
-    if (ret >= mod) ret -= mod;
-    return ret;
-  }
-
-  unsigned connect(unsigned h1, int h2, int h2len) const {
-    unsigned ret = mul(h1, power[h2len]) + h2;
-    if (ret >= mod) ret -= mod;
-    return ret;
-  }
-
-  int LCP(const RollingHash<mod> &b, int l1, int r1, int l2, int r2) {
-    int len = min(r1 - l1, r2 - l2);
-    int low = -1, high = len + 1;
-    while (high - low > 1) {
-      int mid = (low + high) / 2;
-      if (get(l1, l1 + mid) == b.get(l2, l2 + mid))
-        low = mid;
-      else
-        high = mid;
-    }
-    return (low);
-  }
-};
-
-using RH = RollingHash<1000000007>;
-
 int main() {
   cin >> N >> S;
 
-  RH rh(S);
+  vector<vector<int>> dp(N + 1, vector<int>(N + 1, 0));
 
-  auto tooSmall = [&rh](int l) {
-    unordered_map<int, int> m;
-    for (int i = 0; i + l <= N; ++i) {
-      int h = rh.get(i, i + l);
-      auto it = m.find(h);
-      if (it == m.end()) {
-        m[h] = i;
-      } else {
-        if ((*it).second + l <= i) return true;
-      }
-    }
-    return false;
-  };
-
-  int l = 0, r = N / 2 + 1;
-  while (r - l > 1) {
-    int m = (l + r) / 2;
-    if (tooSmall(m)) {
-      l = m;
-    } else {
-      r = m;
+  for (int i = N - 1; i >= 0; --i) {
+    for (int j = N - 1; j >= 0; --j) {
+      if (S[i] == S[j]) dp[i][j] = 1 + dp[i + 1][j + 1];
     }
   }
 
-  cout << l << endl;
+  int ans = 0;
+  rep(i, N) rep(j, N) {
+    if (i >= j) continue;
+    ans = max(ans, min(dp[i][j], j - i));
+  }
+
+  cout << ans << endl;
+
   return 0;
 }
