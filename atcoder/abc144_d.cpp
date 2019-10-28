9cfc8f838498c2853f497bd8ed58876e52333b0d
en30 <en30.git@gmail.com>
2019-10-29 05:15:06 +0900

    ABC144 D O(1)

--- atcoder/abc144_d.cpp
+++ atcoder/abc144_d.cpp
@@ -1,30 +1,21 @@
 #include <bits/stdc++.h>
 #include "../include/template"
 
-int a, b, x;
+double a, b, x;
 
 int main() {
   cin >> a >> b >> x;
 
-  double l = 0, r = M_PI / 2.0 - 1e-7;
-
-  while (r - l > 1e-8) {
-    double m = (l + r) / 2;
-    double v;
-    if (a * tan(m) > b) {
-      v = a * b * b / tan(m) * 0.5;
-    } else {
-      v = a * a * b - a * a * a * tan(m) * 0.5;
-    }
-
-    if (v >= double(x)) {
-      l = m;
-    } else {
-      r = m;
-    }
+  double s = x / a;
+  double rad;
+  if (s >= a * b / 2) {
+    rad = atan2((b - s / a) * 2, a);
+  } else {
+    rad = atan2(b, 2 * s / b);
   }
+  double PI = acos(-1);
+  double ans = rad / PI * 180.0;
 
-  double ans = l / M_PI * 180.0;
   cout << setprecision(7) << ans << endl;
   return 0;
 }
