2907292561fc2c23b59873feb751868f8ffdeaab
en30 <en30.git@gmail.com>
2019-10-29 18:10:51 +0900

    DPまとめコン K 単純化

--- atcoder/dp_k.cpp
+++ atcoder/dp_k.cpp
@@ -2,23 +2,22 @@
 #include "../include/template"
 
 int N, K;
-vector<int> a, grundy;
+vector<int> a, win;
 int main() {
   cin >> N >> K;
-  a.resize(N), grundy.resize(K + 1, 0);
+  a.resize(N), win.resize(K + 1, 0);
   rep(i, N) cin >> a[i];
 
-  rep(k, K + 1) {
-    set<int> s;
+  win[0] = false;
+
+  for (int k = 1; k <= K; ++k) {
+    win[k] = false;
     for (int i = 0; i < N && k - a[i] >= 0; ++i) {
-      s.insert(grundy[k - a[i]]);
+      win[k] = win[k] || !win[k - a[i]];
     }
-    int g = 0;
-    while (s.find(g) != s.end()) ++g;
-    grundy[k] = g;
   }
 
-  cout << (grundy[K] ? "First" : "Second") << endl;
+  cout << (win[K] ? "First" : "Second") << endl;
 
   return 0;
 }
