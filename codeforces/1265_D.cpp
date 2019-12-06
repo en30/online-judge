10d53abd732ec265a653989390f4d7117c348ccb
en30 <en30.git@gmail.com>
2019-12-07 02:41:16 +0900

    Codeforces Round #604 (Div.2) D greedy search using the graph

--- codeforces/1265_D.cpp
+++ codeforces/1265_D.cpp
@@ -1,56 +1,47 @@
 #include <bits/stdc++.h>
 #include "../include/template"
 
-int a, b, c, d;
+int cnt[4];
+
+vector<vector<int>> G;
+
+bool simulate(int i) {
+  --cnt[i];
+  for (int j : G[i]) {
+    if (cnt[j] == 0) continue;
+    bool valid = simulate(j);
+    ++cnt[i];
+    if (valid) cout << " " << i;
+    return valid;
+  }
+
+  bool valid = true;
+  rep(i, 4) valid &= (cnt[i] == 0);
+  if (valid) {
+    cout << "YES" << endl;
+    cout << i;
+  }
+  ++cnt[i];
+  return valid;
+}
 
 int main() {
-  cin >> a >> b >> c >> d;
+  rep(i, 4) cin >> cnt[i];
 
-  if (a - b == 1) {
-    if (c == 0 && d == 0) {
-      cout << "YES" << endl;
-      cout << "0";
-      rep(i, b) cout << " 1 0";
-    } else {
-      cout << "NO";
+  G.resize(4);
+  rep(i, 4) {
+    if (i - 1 >= 0) G[i].push_back(i - 1);
+    if (i + 1 < 4) G[i].push_back(i + 1);
+  }
+
+  rep(i, 4) {
+    if (simulate(i)) {
+      cout << endl;
+      return 0;
     }
-    cout << endl;
-    return 0;
   }
 
-  if (d - c == 1) {
-    if (a == 0 && b == 0) {
-      cout << "YES" << endl;
-      cout << "3";
-      rep(i, c) cout << " 2 3";
-    } else {
-      cout << "NO";
-    }
-    cout << endl;
-    return 0;
-  }
-
-  if (b < a || c < d || abs(a - b + c - d) > 1) {
-    cout << "NO" << endl;
-    return 0;
-  }
-
-  cout << "YES" << endl;
-
-  bool out = false;
-
-  auto p = [&](string s) {
-    if (out) cout << " ";
-    cout << s;
-    out = true;
-  };
-
-  if (b - a > c - d) p("1");
-  rep(i, a) p("0 1");
-  rep(i, min(b - a, c - d)) p("2 1");
-  rep(i, d) p("2 3");
-  if (b - a < c - d) p("2");
-  cout << endl;
+  cout << "NO" << endl;
 
   return 0;
 }
