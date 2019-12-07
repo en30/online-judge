e1a9a254a805404be5cd0839c7d6000dae489f55
en30 <en30.git@gmail.com>
2019-12-07 22:15:23 +0900

    Codeforces Round #604 (Div.2) F

---
+++ codeforces/1265_F.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include "../include/template"
+#include "../include/mod.hpp"
+
+using mint = ModInt<998244353>;
+
+string S;
+
+int main() {
+  cin >> S;
+
+  int N = S.size();
+
+  int M = 0;
+  rep(i, N) M += (S[i] == '?');
+
+  vector<int> L(N + 1, 0), R(N + 1, 0);
+  rep(i, N) {
+    L[i + 1] = L[i] + (S[i] != ')');
+    R[N - i - 1] = R[N - i] + (S[N - i - 1] == ')');
+  }
+
+  vector<mint> fact(M + 1), ifact(M + 1);
+  fact[0] = 1;
+  rep(i, M) fact[i + 1] = fact[i] * (i + 1);
+  rep(i, M + 1) ifact[i] = fact[i].inverse();
+
+  auto comb = [&](int n, int k) { return fact[n] * ifact[n - k] * ifact[k]; };
+
+  vector<mint> SB(M + 2, 0), SQ(M + 1, 0);
+  rep(i, M + 1) SB[i + 1] = SB[i] + comb(M, i);
+  rep(i, M) SQ[i + 1] = SQ[i] + comb(M - 1, i);
+
+  mint ans = 0;
+  rep(i, N) {
+    if (S[i] == ')') continue;
+
+    int k = L[i + 1] - R[i + 1];
+
+    if (S[i] == '(') ans += SB[M + 1] - SB[min(M + 1, max(k, 0))];
+    if (S[i] == '?') ans += SQ[M] - SQ[min(M, max(k, 0))];
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
