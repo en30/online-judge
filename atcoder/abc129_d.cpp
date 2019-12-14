cf9cb35bd4520bf9e7ca3dfc2d1e78334da38b00
en30 <en30.git@gmail.com>
2019-12-15 02:20:13 +0900

    ABC129 D O(HW)

--- atcoder/abc129_d.cpp
+++ atcoder/abc129_d.cpp
@@ -9,22 +9,41 @@ int main() {
   vector<vector<char>> grid(H + 2, vector<char>(W + 2, '#'));
   rep(i, H) rep(j, W) cin >> grid[i + 1][j + 1];
 
-  vector<set<int>> row(H), col(W);
+  vector<vector<vector<int>>> C(H + 2,
+                                vector<vector<int>>(W + 2, vector<int>(4, 0)));
 
-  rep(i, H) rep(j, W + 2) if (grid[i + 1][j] == '#') row[i].insert(j);
-  rep(j, W) rep(i, H + 2) if (grid[i][j + 1] == '#') col[j].insert(i);
+  for (int i = 1; i <= H; ++i)
+    for (int j = 1; j <= W; ++j) {
+      if (grid[i][j] == '#') {
+        C[i][j][0] = 0;
+      } else {
+        C[i][j][0] = C[i][j - 1][0] + 1;
+      }
+      if (grid[i][W - j + 1] == '#') {
+        C[i][W - j + 1][1] = 0;
+      } else {
+        C[i][W - j + 1][1] = C[i][W - j + 2][1] + 1;
+      }
+    }
+
+  for (int j = 1; j <= W; ++j)
+    for (int i = 1; i <= H; ++i) {
+      if (grid[i][j] == '#') {
+        C[i][j][2] = 0;
+      } else {
+        C[i][j][2] = C[i - 1][j][2] + 1;
+      }
+      if (grid[H - i + 1][j] == '#') {
+        C[H - i + 1][j][3] = 0;
+      } else {
+        C[H - i + 1][j][3] = C[H - i + 2][j][3] + 1;
+      }
+    }
 
   int ans = 0;
   rep(i, H) rep(j, W) {
     if (grid[i + 1][j + 1] == '#') continue;
-
-    int c = 0;
-    auto it = row[i].upper_bound(j + 1);
-    c += (*it) - (*prev(it)) - 1;
-    it = col[j].upper_bound(i + 1);
-    c += (*it) - (*prev(it)) - 1;
-    c -= 1;
-    ans = max(ans, c);
+    ans = max(ans, accumulate(all(C[i][j]), 0) - 3);
   }
 
   cout << ans << endl;
