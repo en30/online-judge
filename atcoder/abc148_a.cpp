e3fb19ec816b3cb086314ac98413f4cfedc3f4e9
en30 <en30.git@gmail.com>
2019-12-24 22:43:44 +0900

    ABC148 rewrite A, F

--- atcoder/abc148_a.cpp
+++ atcoder/abc148_a.cpp
@@ -4,10 +4,7 @@
 int A, B;
 int main() {
   cin >> A >> B;
-  set<int> s = {1, 2, 3};
-  s.erase(A);
-  s.erase(B);
-  cout << *s.begin() << endl;
+  cout << 6 - A - B << endl;
 
   return 0;
 }
--- atcoder/abc148_f.cpp
+++ atcoder/abc148_f.cpp
@@ -1,42 +1,57 @@
 #include <bits/stdc++.h>
 #include "../include/template"
 
-int N, u, v, M;
-vector<vector<int>> G;
+class UndirectedGraph {
+ public:
+  int V, E;
+  vector<vector<int>> adj;
+  UndirectedGraph(int V) : V(V), E(0) { adj.resize(V); }
 
-vector<int> bfs(int u) {
-  vector<int> d(N, -1);
-  queue<int> q;
-  q.push(u);
-  d[u] = 0;
-  while (!q.empty()) {
-    int v = q.front();
-    q.pop();
-    for (int w : G[v]) {
-      if (d[w] != -1) continue;
-      d[w] = d[v] + 1;
-      q.push(w);
-    }
+  void addEdge(int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+    ++E;
   }
-  return d;
-}
+
+  int deg(int u) { return adj[u].size(); }
+
+  vector<int> singleSourceShortestPath(int s) {
+    vector<int> d(V, -1);
+    queue<int> q;
+    q.push(s);
+    d[s] = 0;
+    while (!q.empty()) {
+      int v = q.front();
+      q.pop();
+      for (int w : adj[v]) {
+        if (d[w] != -1) continue;
+        d[w] = d[v] + 1;
+        q.push(w);
+      }
+    }
+    return d;
+  }
+};
+
+int N, u, v, M;
 
 int main() {
   cin >> N >> u >> v;
   --u, --v;
-  G.resize(N);
+
+  UndirectedGraph g(N);
 
   rep(i, N - 1) {
     int A, B;
     cin >> A >> B;
     --A, --B;
-    G[A].push_back(B);
-    G[B].push_back(A);
+    g.addEdge(A, B);
   }
 
-  vector<int> dT = bfs(u), dA = bfs(v);
+  vector<int> dT = g.singleSourceShortestPath(u),
+              dA = g.singleSourceShortestPath(v);
 
-  if (G[u].size() == 1 && G[u][0] == v) {
+  if (g.deg(u) == 1 && g.adj[u][0] == v) {
     cout << 0 << endl;
     return 0;
   }
