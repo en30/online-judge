7f00e9edb05a4c5b052db4e41edc8d3fb5e0f733
en30 <en30.git@gmail.com>
2019-12-25 04:15:18 +0900

    Codeforces Round #610 (Div. 2) simplifiy B

--- codeforces/1282_B2.cpp
+++ codeforces/1282_B2.cpp
@@ -9,37 +9,21 @@ void solve() {
   vector<int> a(n);
   rep(i, n) cin >> a[i];
   sort(all(a));
-  vector<ll> S(n + 1, 0);
-  rep(i, n) S[i + 1] = S[i] + a[i];
-  vector<vector<ll>> T(k, vector<ll>(n / k + 10, 0));
-  rep(i, k) for (ll j = 0; i + j * k < n; ++j) {
-    T[i][j + 1] = T[i][j] + a[i + j * k];
-  }
 
-  auto ok = [&](int i, ll j) {
-    if (i + j * k > n) return false;
-    int d = (i - 1 + k) % k;
-
-    return S[i] + T[d][(i - 1 + j * k) / k + 1] - T[d][(i - 1 + k) / k] <= p;
-  };
-
-  int ans = 0;
-  rep(i, n + 1) {
-    if (p - S[i] < 0) break;
-
-    int l = 0, r = 1e6;
-    while (r - l > 1) {
-      int m = (l + r) / 2;
-      if (ok(i, m)) {
-        l = m;
-      } else {
-        r = m;
-      }
+  vector<ll> dp(n + 1, 0);
+  rep(i, n) {
+    if (i < k - 1) {
+      dp[i + 1] = dp[i] + a[i];
+    } else {
+      dp[i + 1] = min(dp[i], dp[i - k + 1]) + a[i];
     }
-    ans = max(ans, i + l * k);
   }
 
-  cout << ans << endl;
+  for (int i = n; i >= 0; --i) {
+    if (dp[i] > p) continue;
+    cout << i << endl;
+    return;
+  }
 }
 
 int main() {
