7e47314804591602d11faa4ca8364a17556eca47
en30 <en30.git@gmail.com>
2019-12-31 22:12:50 +0900

    ABC007 simplify D

--- atcoder/abc007_4.cpp
+++ atcoder/abc007_4.cpp
@@ -3,23 +3,18 @@
 
 string A, B;
 
-vector<int> cnt = {0, 0, 0, 0, 1, 1, 1, 1, 1, 2};
-
-int bad(char c) { return c == '4' || c == '9'; }
-
 ll count(string s) {
   int n = s.size();
   vector<vector<vector<ll>>> dp(n + 1, vector<vector<ll>>(2, vector<ll>(2, 0)));
   dp[0][0][0] = 1;
 
   rep(i, n) {
-    int d = s[i] - '0';
-    dp[i + 1][1][1] = 10 * dp[i][1][1] + 2 * dp[i][1][0] + d * dp[i][0][1] +
-                      (d == 0 ? 0 : cnt[d - 1] * dp[i][0][0]);
-    dp[i + 1][1][0] =
-        8 * dp[i][1][0] + (d == 0 ? 0 : (d - cnt[d - 1]) * dp[i][0][0]);
-    dp[i + 1][0][1] = dp[i][0][1] + (bad(s[i])) * dp[i][0][0];
-    dp[i + 1][0][0] = (!bad(s[i])) * dp[i][0][0];
+    rep(j, 2) rep(k, 2) {
+      int d = s[i] - '0';
+      rep(l, (j ? 10 : d + 1)) {
+        dp[i + 1][j || l < d][k || l == 4 || l == 9] += dp[i][j][k];
+      }
+    }
   }
 
   return dp[n][0][1] + dp[n][1][1];
