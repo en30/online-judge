ff7a5ed3a553128e5f9bc321780d07671f81f5d9
en30 <en30.git@gmail.com>
2020-01-21 03:15:34 +0900

    ABC152 Inclusion-exclusion principle

--- atcoder/abc152_f.cpp
+++ atcoder/abc152_f.cpp
@@ -1,54 +1,63 @@
 #include <bits/stdc++.h>
 #include "../include/template"
 
+using EdgeSet = bitset<50>;
+
 int N;
+
+struct Edge {
+  int to, id;
+  Edge(int to, int id): to(to), id(id) {}
+};
+
+vector<vector<Edge>> G;
 
 int main() {
   cin >> N;
-  vector<vector<int>> G(N);
-  vector<bitset<20>> C(N - 1, 0);
-  unordered_map<int, unordered_map<int, int>> edge;
+  G.resize(N);
   rep(i, N - 1) {
     int a, b;
     cin >> a >> b;
     --a, --b;
-    G[a].push_back(b);
-    G[b].push_back(a);
-    edge[a][b] = i;
-    edge[b][a] = i;
+    G[a].emplace_back(b, i);
+    G[b].emplace_back(a, i);
   }
 
   int M;
   cin >> M;
 
-  function<bool(int, int, int, int)> append = [&](int u, int v, int p, int c) {
-    if (u == v) return true;
+  vector<EdgeSet> edges(M);
 
-    bool found = false;
-    for (auto w : G[u]) {
-      if (w == p) continue;
-      bool goodPath = append(w, v, u, c);
-      if (goodPath) C[edge[u][w]].set(c);
-      found |= goodPath;
+  function<bool(int, int,int,int)> dfs = [&](int c, int v, int tv, int p) {
+    if(v == tv) return true;
+    for(Edge& e: G[v]) {
+      if(e.to == p) continue;
+      if(dfs(c, e.to, tv, v)) {
+        edges[c].set(e.id);
+        return true;
+      }
     }
-    return found;
+    return false;
   };
 
   rep(i, M) {
     int u, v;
     cin >> u >> v;
     --u, --v;
-    append(u, v, -1, i);
+    dfs(i, u, v, -1);
   }
 
-  vector<vector<ll>> dp((1 << M), vector<ll>(N, 0));
-  dp[0][0] = 1;
-  rep(i, N - 1) rep(s, (1 << M)) {
-    dp[s | int(C[i].to_ulong())][i + 1] += dp[s][i];  // B
-    dp[s][i + 1] += dp[s][i];                         // W
+  ll ans = 0;
+  rep(subset,1<<M) {
+    bitset<50> shouldWhite;
+    rep(c,M) {
+      if ((subset >> c) & 1) shouldWhite |= edges[c];
+    }
+    ll m = 1LL << (N - 1 - shouldWhite.count());
+    if(__builtin_popcountll(subset) % 2 == 0) ans += m;
+    else ans -= m;
   }
 
-  cout << dp[(1 << M) - 1][N - 1] << endl;
-
+  cout << ans << endl;
   return 0;
 }
