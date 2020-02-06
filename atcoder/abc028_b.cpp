02c44fd3dfcb78584ae747ace1f8cbcd0736a290
en30 <en30.git@gmail.com>
2020-02-07 03:43:55 +0900

    refines ABC028

--- atcoder/abc028_b.cpp
+++ atcoder/abc028_b.cpp
@@ -2,17 +2,12 @@
 #include "../include/template"
 
 int main() {
-  map<char, int> C;
-  rep(i, 6) C['A' + i] = 0;
   string S;
   cin >> S;
-  for (char c : S) C[c] += 1;
-
-  for (auto it = C.begin(); it != C.end(); ++it) {
-    if (it != C.begin()) cout << " ";
-    cout << it->second;
+  rep(i, 6) {
+    if (i) cout << " ";
+    cout << count(all(S), 'A' + i);
   }
   cout << endl;
-
   return 0;
 }
