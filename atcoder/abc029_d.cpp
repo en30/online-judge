0603facd78f1336d6e06e44f64bd7d97b4c2774a
en30 <en30.git@gmail.com>
2020-02-08 04:37:27 +0900

    ABC029 D without DP

--- atcoder/abc029_d.cpp
+++ atcoder/abc029_d.cpp
@@ -2,26 +2,15 @@
 #include "../include/template"
 
 int main() {
-  string N;
+  int N;
   cin >> N;
-  const int M = N.size();
-  vector<vector<vector<int>>> dp(
-      2, vector<vector<int>>(M + 1, vector<int>(10, 0)));
-  dp[0][0][0] = 1;
-  rep(i, M) {
-    int m = N[i] - '0';
-    rep(k, 10) {
-      if (k + (m == 1) < 10) dp[0][i + 1][k + (m == 1)] += dp[0][i][k];
-      dp[1][i + 1][k] += (m - (m > 1)) * dp[0][i][k];
-      if (m > 1) dp[1][i + 1][k + 1] += dp[0][i][k];
-      dp[1][i + 1][k] += 9 * dp[1][i][k];
-      dp[1][i + 1][k + 1] += dp[1][i][k];
-    }
-  }
-
   int ans = 0;
-  rep(i, 2) rep(j, 10) ans += j * dp[i][M][j];
+  int t = 10;
+  rep(i, 9) {
+    ans += (t / 10) * (N / t) + min(t / 10, max(N % t + 1 - t / 10, 0));
+    t *= 10;
+  }
   cout << ans << endl;
-
+  
   return 0;
 }
