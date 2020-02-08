1c8636ec2083d8a3238fef4ef445a91e38041646
en30 <en30.git@gmail.com>
2020-02-09 02:09:39 +0900

    refines ABC030 D

--- atcoder/abc030_d.cpp
+++ atcoder/abc030_d.cpp
@@ -11,21 +11,16 @@ int main() {
   vector<int> b(N + 1);
   rep(i, N) cin >> b[i + 1];
 
-  vector<int> path;
-  int c = a;
-  vector<bool> used(N + 1, false);
-  while (true) {
-    used[c] = true;
-    path.push_back(c);
-    c = b[c];
-    if (used[c]) break;
-  }
-  int r = 0;
-  while (path[r] != c) ++r;
-  const int T = path.size() - r;
-  const int M = k.size();
+  vector<int> path(N + 1, a);
+  rep(i, N) path[i + 1] = b[path[i]];
 
-  if (M <= 6 && stoi(k) <= r) {
+  int T = 1;
+  while (path[N - T] != path[N]) ++T;
+  int r = 0;
+  while (path[r] != path[r + T]) ++r;
+
+  const int M = k.size();
+  if (M <= 6 && stoi(k) < N) {
     cout << path[stoi(k)] << endl;
     return 0;
   }
@@ -34,7 +29,6 @@ int main() {
   ModInt t = 0;
   rep(i, M) t = t * 10 + (k[i] - '0');
   t -= r;
-
   cout << path[r + t.val()] << endl;
 
   return 0;
