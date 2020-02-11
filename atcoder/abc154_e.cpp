735a632d6fa9baf332b85fe7e9edf39c8d020199
en30 <en30.git@gmail.com>
2020-02-12 04:35:54 +0900

    rewrites transition of ABC154 E using loop

--- atcoder/abc154_e.cpp
+++ atcoder/abc154_e.cpp
@@ -12,12 +12,17 @@ int main() {
                                 vector<vector<ll>>(K + 2, vector<ll>(2, 0)));
   dp[0][0][0] = 1;
 
-  rep(i, M) rep(j, K + 1) {
-    dp[i + 1][j + (N[i] != '0')][0] += dp[i][j][0];
-    if (N[i] != '0') dp[i + 1][j][1] += dp[i][j][0];
-    dp[i + 1][j + 1][1] += (N[i] - '0' - (N[i] >= '1')) * dp[i][j][0];
-    dp[i + 1][j][1] += dp[i][j][1];
-    dp[i + 1][j + 1][1] += 9 * dp[i][j][1];
+  rep(i, M) rep(j, K + 1) rep(k, 2) {
+    int nd = N[i] - '0';
+    rep(d, 10) {
+      int ni = i + 1, nj = j, nk = k;
+      if (d != 0) ++nj;
+      if (k == 0) {
+        if (d > nd) continue;
+        if (d < nd) nk = 1;
+      }
+      dp[ni][nj][nk] += dp[i][j][k];
+    }
   }
 
   cout << dp[M][K][0] + dp[M][K][1] << endl;
