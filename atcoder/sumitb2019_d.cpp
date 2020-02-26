43eebf32c2c745d2ce5730c07ffcc5bf450f2792
en30 <en30.git@gmail.com>
2020-02-27 02:58:52 +0900

    atcoder/sumitb2019_d.cpp DP O(300N)

--- atcoder/sumitb2019_d.cpp
+++ atcoder/sumitb2019_d.cpp
@@ -7,17 +7,14 @@ int main() {
   string S;
   cin >> S;
 
-  int ans = 0;
-  rep(i, 10) rep(j, 10) rep(k, 10) {
-    vector<char> c = {i + '0', j + '0', k + '0'};
-    int m = -1;
-    rep(l, 3) {
-      ++m;
-      while (m < N && S[m] != c[l]) ++m;
-    }
-    ans += (m < N);
-  };
-  cout << ans << endl;
+  vector<vector<bool>> dp(4, vector<bool>(1000, false));
+  dp[0][0] = true;
+  rep(i, N) for (int j = 2; j >= 0; --j) for (int k = 99; k >= 0; --k) {
+    if (!dp[j][k]) continue;
+    dp[j + 1][k * 10 + (S[i] - '0')] = true;
+  }
+
+  cout << accumulate(all(dp[3]), 0) << endl;
 
   return 0;
 }
