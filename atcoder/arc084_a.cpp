24b631d5a8b7bc441f45931931efbd6eb499d590
en30 <en30.git@gmail.com>
2020-02-29 03:29:55 +0900

    rewrites ABC077 C - Snuke Festival

--- atcoder/arc084_a.cpp
+++ atcoder/arc084_a.cpp
@@ -1,35 +1,23 @@
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define rep(i, N) for (int i = 0; i < (int)N; i++)
-#define all(a) (a).begin(), (a).end()
+#include "../template"
 
-int N;
-vector<int> A, B, C;
-
-int main () {
+int main() {
+  int N;
   cin >> N;
-  A.resize(N), B.resize(N), C.resize(N);
-  rep(i,N) cin >> A[i];
-  rep(i,N) cin >> B[i];
-  rep(i,N) cin >> C[i];
+  vector<int> A(N), B(N), C(N);
+  rep(i, N) cin >> A[i];
+  rep(i, N) cin >> B[i];
+  rep(i, N) cin >> C[i];
 
-  sort(all(A)), sort(all(B)), sort(all(C));
-
-  vector<ll> BC(N, 0);
-  rep(i,N) {
-    BC[i] = distance(upper_bound(all(C), B[i]), C.end());
-  }
-  for(int i = N-1; i > 0; --i) {
-    BC[i-1] += BC[i];
-  }
+  sort(all(A));
+  sort(all(C));
 
   ll ans = 0;
-  rep(i,N) {
-    int j = distance( B.begin(), upper_bound(all(B), A[i]));
-    if(j < N) ans += BC[j];
+  rep(i, N) {
+    ll a = distance(A.begin(), lower_bound(all(A), B[i]));
+    ll c = distance(upper_bound(all(C), B[i]), C.end());
+    ans += a * c;
   }
-
   cout << ans << endl;
 
   return 0;
