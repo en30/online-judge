45a4b40589dea205f3ed38e33d9014c0df8bd5ab
en30 <en30.git@gmail.com>
2020-03-02 01:19:32 +0900

    refines ABC157

--- atcoder/abc157_e.cpp
+++ atcoder/abc157_e.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 #include "../include/template"
-#include "../include/segment_tree.hpp"
 
 int main() {
   int N;
@@ -8,13 +7,12 @@ int main() {
   string S;
   cin >> S;
 
+  const int R = 26;
+  vector<set<int>> idx(R);
+  rep(i, N) idx[S[i] - 'a'].insert(i);
+
   int Q;
   cin >> Q;
-
-  SegmentTree<int> seg(
-      N, [](int a, int b) { return a | b; }, 0);
-  rep(i, N) { seg.update(i, 1 << (S[i] - 'a')); }
-
   rep(i, Q) {
     int t;
     cin >> t;
@@ -23,16 +21,20 @@ int main() {
       char c;
       cin >> i >> c;
       --i;
-      int s = seg[i];
-      s = s & ~(1 << (S[i] - 'a'));
-      s |= 1 << (c - 'a');
+      idx[S[i] - 'a'].erase(i);
       S[i] = c;
-      seg.update(i, s);
+      idx[c - 'a'].insert(i);
     } else {
       int l, r;
       cin >> l >> r;
       --l, --r;
-      cout << __builtin_popcount(seg.query(l, r + 1)) << endl;
+      int ans = 0;
+      rep(c, R) {
+        auto it = idx[c].lower_bound(l);
+        if (it == idx[c].end()) continue;
+        if (*it <= r) ++ans;
+      }
+      cout << ans << endl;
     }
   }
   return 0;
