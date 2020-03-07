9020e2fa9b2f467def64a1fcceb0fc4028f8460e
en30 <en30.git@gmail.com>
2020-03-08 04:06:43 +0900

    refines ABC158 E

--- atcoder/abc158_e.cpp
+++ atcoder/abc158_e.cpp
@@ -7,33 +7,30 @@ int main() {
   string S;
   cin >> S;
 
+  reverse(all(S));
   if (P == 2 || P == 5) {
-    vector<vector<ll>> dp(N + 1, vector<ll>(P, 0));
-    rep(i, N) rep(p, P) {
-      dp[i + 1][(p * 10 + (S[i] - '0')) % P] += dp[i][p] + (p == 0);
-    }
-    ll s = 0;
-    rep(i, N + 1) s += dp[i][0];
-    cout << s << endl;
-  } else {
-    vector<ll> T(N + 1, 0);
-    reverse(all(S));
-    ll r = 1;
-    rep(i, N) {
-      T[i + 1] = T[i] + r * (S[i] - '0');
-      T[i + 1] %= P;
-      r *= 10;
-      r %= P;
-    }
-
     ll ans = 0;
-    map<ll, ll> C;
-    rep(i, N + 1) {
-      ans += C[T[i]];
-      ++C[T[i]];
-    }
+    rep(i, N) if ((S[i] - '0') % P == 0) ans += N - i;
     cout << ans << endl;
+    return 0;
   }
+
+  vector<ll> T(N + 1, 0);
+  ll r = 1;
+  rep(i, N) {
+    T[i + 1] = T[i] + r * (S[i] - '0');
+    T[i + 1] %= P;
+    r *= 10;
+    r %= P;
+  }
+
+  ll ans = 0;
+  map<ll, ll> C;
+  rep(i, N + 1) {
+    ans += C[T[i]];
+    ++C[T[i]];
+  }
+  cout << ans << endl;
 
   return 0;
 }
