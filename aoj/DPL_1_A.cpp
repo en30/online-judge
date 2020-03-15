0244fd4865dbdf52d8a1b65c7a109049fdd7e160
en30 <en30.git@gmail.com>
2020-03-15 18:43:27 +0900

    DPL_1_A - コイン問題

--- aoj/DPL_1_A.cpp
+++ aoj/DPL_1_A.cpp
@@ -1,35 +1,17 @@
 #include <bits/stdc++.h>
+#include "../include/template"
 
-using namespace std;
-typedef long long ll;
-
-#define REP(i, N) for (int i = 0; i < (int)N; i++)
-
-#define M 20
-#define N 50001
-
-int d[M];
-int DP[M][N];
-
-int main () {
+int main() {
   int n, m;
-
   cin >> n >> m;
-  REP(i,M) cin >> d[i];
+  vector<int> c(m);
+  rep(i, m) cin >> c[i];
 
-  for(int x = 0; x <= n; x++) {
-    for(int i = 0; i < m; i++) DP[i][x] = N;
-  }
-  for(int k = 0, kMax = n / d[0]; k <= kMax; k++) DP[0][k*d[0]] = k;
+  vector<int> dp(n + 1, 1e9);
+  dp[0] = 0;
 
-  for(int i = 1; i < m; i++) {
-    for(int x = 0; x <= n; x++) {
-      DP[i][x] = DP[i-1][x];
-      if(x - d[i] >= 0)DP[i][x] = min(DP[i][x], DP[i][x- d[i]] + 1);
-    }
-  }
-
-  cout << DP[m-1][n] << endl;
+  rep(i, m) rep(j, n + 1) if (j - c[i] >= 0) chmin(dp[j], dp[j - c[i]] + 1);
+  cout << dp[n] << endl;
 
   return 0;
 }
