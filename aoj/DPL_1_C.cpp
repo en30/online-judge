55d90e1149adb7f18373d1f42bb08f1474e5e245
en30 <en30.git@gmail.com>
2020-03-15 18:36:58 +0900

    DPL_1_C - ナップザック問題

--- aoj/DPL_1_C.cpp
+++ aoj/DPL_1_C.cpp
@@ -1,28 +1,16 @@
 #include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define REP(i, N) for (int i = 0; i < (int)N; i++)
+#include "../include/template"
 
-#define N_MAX 100
-#define W_MAX 10000
-
-int dp[W_MAX+1];
-int v[N_MAX];
-int w[N_MAX];
-
-int main () {
+int main() {
   int N, W;
   cin >> N >> W;
-  REP(i,N) cin >> v[i] >> w[i];
-  fill(dp, dp + W_MAX + 1, 0);
 
-  REP(i,N) {
-    for(int j = 0; j <= W; j++) {
-      if(j - w[i] >= 0) dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
-    }
-  }
+  vector<int> v(N), w(N);
+  rep(i, N) cin >> v[i] >> w[i];
+
+  vector<int> dp(W + 1, 0);
+  rep(i, N) rep(j, W + 1) if (j - w[i] >= 0) chmax(dp[j], dp[j - w[i]] + v[i]);
   cout << dp[W] << endl;
-
 
   return 0;
 }
